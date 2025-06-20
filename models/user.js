6bf2265f2aa606334646e447d2b24238cb74806a
Crystlfly <anusha.singh.hca@gmail.com>
2025-06-20 12:21:09 +0530

    Fixed: user registration

--- models/user.js
+++ models/user.js
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema(
     },
     profileImage: {
       type: String,
-      required: true,
+      // required: true,
     },
     profileCompleted: {
       type: Boolean,
