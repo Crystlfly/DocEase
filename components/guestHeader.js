036f26fff4ddca4afbc9d3769abe039bdfb54049
Crystlfly <anusha.singh.hca@gmail.com>
2025-08-21 16:24:28 +0530

    Updating

--- components/guestHeader.js
+++ components/guestHeader.js
@@ -1,6 +1,6 @@
-import React from 'react'; // ✅ required
+import React from 'react'; 
 import Link from "next/link";
-import styles from "@/styles/PatientHeader.module.css"; // Reuse existing styles
+import styles from "@/styles/PatientHeader.module.css"; 
 
 
 
